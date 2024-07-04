9b862328daeb9c41c392eeb09f272bc4c08e49e0
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-04 20:31:32 +0200

    9 fix

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,5 @@ from typing import Iterable, Sequence, Tuple, List
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """a function to check the length of the argument"""
     return [(i, len(i)) for i in lst]
