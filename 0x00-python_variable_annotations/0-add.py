c953f4cd8cc82e7d01882a512f4fe607d7075bd3
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-04 16:47:36 +0200

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""add two floats with typing-annotations included"""
+
+
+def add(a: float, b: float) -> float:
+    """Method to ad two floats and return a float
+       Args:
+           a(float): first parameter
+           b(float): second parameter
+    """
+    return (a + b)
