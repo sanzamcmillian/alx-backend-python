5d027c19d1b14e903cc8fe9653fc9a542cda0ff1
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-04 16:54:51 +0200

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""concatenate two string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """a function to add two strings together
+        Args:
+            str1(string): first string
+            str2(string): second string
+    """
+    return (str1 + str2)
