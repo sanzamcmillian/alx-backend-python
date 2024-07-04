e57cf25a4c19d0ac0a5430b89cb140f610cd464b
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-04 17:13:35 +0200

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""float to string representation"""
+
+
+def to_str(n: float) -> str:
+    """a function to return a float into a string
+       Args:
+           n(float): float to be changed
+    """
+    return (str(n))
