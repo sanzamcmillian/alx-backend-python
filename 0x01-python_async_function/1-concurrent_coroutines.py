f3d53ee4bf7a7124d7d380c7bbca024beac44049
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-08 16:47:46 +0200

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 """get a list of the waits in ascending order"""
-__import__('0-basic_async_syntax.')
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay)
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """wait random in a list"""
+    hold = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(hold)
