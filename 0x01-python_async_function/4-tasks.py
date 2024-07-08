20c813ad9bd201fe2a10478b24bc728e4f5cd6bd
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-08 17:18:06 +0200

    task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import asyncio
+from typing import List
+"""call task wait random"""
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """task wait random in a list"""
+    hold = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(hold)
