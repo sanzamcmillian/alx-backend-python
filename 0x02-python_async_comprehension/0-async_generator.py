c59c2944ce017cde2a8c589541e143fff3a54936
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-09 11:45:23 +0200

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""async generator"""
+import random
+import asyncio
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None]:
+    """a coroutine to yield randmon numbers"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
